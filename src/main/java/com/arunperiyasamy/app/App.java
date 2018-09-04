d16cce0c522349f1438cbda1871d53e4de566414
arunperiyasamy <arunperiyasamy96@gmail.com>
2018-09-04 12:10:35 +0530

    file added

---
+++ src/main/java/com/arunperiyasamy/app/App.java
@@ -0,0 +1,13 @@
+package com.arunperiyasamy.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
